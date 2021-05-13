55d5eddb1cc89c063f5384cbf16c547eff67d099
Keith Crabtree <keithcrabtree@live.com>
2021-05-12 21:56:05 -0400

    update to display posts on home page dynamically

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
 class Post {
+    public static function all() {
+        $files = File::files(\resource_path('posts/'));
+ 
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+    }
+
     public static function find($uriSlug) {
         $path = resource_path("posts/{$uriSlug}.html");
 
