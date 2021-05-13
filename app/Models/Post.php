6b0f3db26abda36ce2066221f063ce7895c4747c
Keith Crabtree <keithcrabtree@live.com>
2021-05-12 22:44:51 -0400

    add metadata to posts and update routes to dynamically display content

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,25 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post {
+    public $title;
+    public $publishDate;
+    public $excerpt;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $publishDate, $excerpt, $body, $slug) {
+        $this->title = $title;
+        $this->publishDate = $publishDate;
+        $this->excerpt = $excerpt;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all() {
-        $files = File::files(\resource_path('posts/'));
- 
-        return array_map(function ($file) {
-            return $file->getContents();
-        }, $files);
+        return collect(File::files(resource_path('posts')))
+            ->map(function($file) {
+                return YamlFrontMatter::parseFile($file);
+            })
+            ->map(function($doc) {
+                return new Post($doc->title, $doc->publishDate, $doc->excerpt, $doc->body(), $doc->slug);
+            });
     }
 
     public static function find($uriSlug) {
-        $path = resource_path("posts/{$uriSlug}.html");
+        return static::all()->firstWhere('slug', $uriSlug);
+        // $path = resource_path("posts/{$uriSlug}.html");
 
-        if(!file_exists($path)) {
-            throw new ModelNotFoundException();
-        }
+        // if(!file_exists($path)) {
+        //     throw new ModelNotFoundException();
+        // }
 
-        return cache()->remember("posts.{$uriSlug}", now()->addHour(), function () use ($path) {
-            return file_get_contents($path);
-        });
+        // return cache()->remember("posts.{$uriSlug}", now()->addHour(), function () use ($path) {
+        //     return file_get_contents($path);
+        // });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
