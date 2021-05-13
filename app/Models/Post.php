2d349503c8e146436d361311ec974c2ae98961ae
Keith Crabtree <keithcrabtree@live.com>
2021-05-13 18:02:39 -0400

    remove mock post files and old model. add migration to create posts table, Post eloquent model, and update views to use ids instead of slugs

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,48 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public $title;
-    public $publishDate;
-    public $excerpt;
-    public $body;
-    public $slug;
-
-    public function __construct($title, $publishDate, $excerpt, $body, $slug) {
-        $this->title = $title;
-        $this->publishDate = $publishDate;
-        $this->excerpt = $excerpt;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all() {
-        return cache()->rememberForever('posts.all', function() {
-            return collect(File::files(resource_path('posts')))
-            ->map(function($file) {
-                return YamlFrontMatter::parseFile($file);
-            })
-            ->map(function($doc) {
-                return new Post($doc->title, $doc->publishDate, $doc->excerpt, $doc->body(), $doc->slug);
-            })->sortByDesc('publishDate');
-        });
-    }
-
-    public static function find($uriSlug) {
-        return static::all()->firstWhere('slug', $uriSlug);
-    }
-
-    public static function findOrFail($uriSlug) {
-        $post = static::find($uriSlug);
-
-        if(!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+class Post extends Model
+{
+    use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/', function () {
     return view('posts', ['posts' => Post::all()]);
 });
 
-Route::get('posts/{post}', function ($uriSlug) {
-    return view('post', ['post' => Post::findOrFail($uriSlug)]);
+Route::get('posts/{post}', function ($id) {
+    return view('post', ['post' => Post::findOrFail($id)]);
 });
